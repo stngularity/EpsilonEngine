01dc6572783f1f825e05c47b7a347b152a0e4387
stngularity <stngularity@gmail.com>
2023-09-02 18:03:36 +0300

    Moved template pattern to top

--- src/main/java/io/github/stngularity/epsilon/engine/Constants.java
+++ src/main/java/io/github/stngularity/epsilon/engine/Constants.java
@@ -7,20 +7,6 @@ import java.util.regex.Pattern;
  */
 public class Constants {
     /**
-     * The default value of placeholder pattern
-     */
-    public static final EPattern DEFAULT_PLACEHOLDER_PATTERN = EPattern.builder()
-            .child("start", Pattern.compile("\\{"))
-            .child("name", Pattern.compile("[a-zA-Z0-9_\\-:.]+"))
-            .child("data", true,
-                    EPattern.builder().name("start").regex(Pattern.compile("\\[")).build(),
-                    EPattern.builder().name("value").regex(Pattern.compile("[^]]+"))
-                            .child("escape", Pattern.compile("\\\\.*"), true).build(),
-                    EPattern.builder().name("end").regex(Pattern.compile("]")).build())
-            .child("end", Pattern.compile("}"))
-            .build();
-
-    /**
      * The default value of template pattern
      */
     public static final EPattern DEFAULT_TEMPLATE_PATTERN = EPattern.builder()
@@ -32,6 +18,20 @@ public class Constants {
                             .child("escape", Pattern.compile("\\\\.*"), true).build(),
                     EPattern.builder().name("end").regex(Pattern.compile("]")).build())
             .child("end", Pattern.compile("\\$"))
+            .build();
+
+    /**
+     * The default value of placeholder pattern
+     */
+    public static final EPattern DEFAULT_PLACEHOLDER_PATTERN = EPattern.builder()
+            .child("start", Pattern.compile("\\{"))
+            .child("name", Pattern.compile("[a-zA-Z0-9_\\-:.]+"))
+            .child("data", true,
+                    EPattern.builder().name("start").regex(Pattern.compile("\\[")).build(),
+                    EPattern.builder().name("value").regex(Pattern.compile("[^]]+"))
+                            .child("escape", Pattern.compile("\\\\.*"), true).build(),
+                    EPattern.builder().name("end").regex(Pattern.compile("]")).build())
+            .child("end", Pattern.compile("}"))
             .build();
 
     /**
